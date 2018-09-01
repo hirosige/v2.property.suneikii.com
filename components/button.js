b8af3738ab64a9dcdb49769e04d61e1e9d054045
Hiroshige.Negishi <hirosige1@gmail.com>
2018-09-01 12:53:24 +0700

    upgrade packages

---
+++ components/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const PSButton = () => (
+  <div>
+    <Button variant="contained" color="primary">
+      Material!
+    </Button>
+  </div>
+);
+
+export default PSButton;
