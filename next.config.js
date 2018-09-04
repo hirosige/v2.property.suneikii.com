26c8ab72590fc7e6c98107d1df96ac4ace85bc21
Hiroshige.Negishi <hirosige1@gmail.com>
2018-09-04 20:36:09 +0700

    update config file

--- next.config.js
+++ next.config.js
@@ -7,21 +7,39 @@ const withESLint = require('next-eslint');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
 
-module.exports = withESLint({
-  webpack: (config) => {
-    config.plugins = config.plugins || [];
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    webpack: (config) => {
+      config.plugins = config.plugins || [];
 
-    config.plugins = [
-      ...config.plugins,
+      config.plugins = [
+        ...config.plugins,
 
-      new FlowStatusWebpackPlugin({
-        failOnError: true,
-      }),
-      new Dotenv({
-        path: path.join(__dirname, '.env'),
-        systemvars: true,
-      }),
-    ];
-    return config;
-  },
-});
+        new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true,
+        }),
+      ];
+      return config;
+    },
+  };
+} else {
+  module.exports = withESLint({
+    webpack: (config) => {
+      config.plugins = config.plugins || [];
+
+      config.plugins = [
+        ...config.plugins,
+
+        new FlowStatusWebpackPlugin({
+          failOnError: true,
+        }),
+        new Dotenv({
+          path: path.join(__dirname, '.env'),
+          systemvars: true,
+        }),
+      ];
+      return config;
+    },
+  });
+}
